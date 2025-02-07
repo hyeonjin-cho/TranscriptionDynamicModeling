bb09206889db164d90679a2d883f91a9daaec624
Hyeon Jin Cho <57103534+hyeonjin-cho@users.noreply.github.com>
2025-02-06 22:04:36 -0500

    Create makeswarm.sh

---
+++ makeswarm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# Check if the genes.txt file exists
+if [ ! -f path/to/genelist.txt ]; then
+    echo "file not found!"
+    exit 1
+fi
+
+swarm_file="fit_scRNA.swarm"
+
+while IFS= read -r gene; do
+    # Submit the job for each gene
+   echo "julia -t 2 -p 11 fitscript_scRNA.jl      $gene > swarm_outputs/$gene.o" >> "$swarm_file"
+
+done < path/to/genelist.txt
+
+echo "Swarm file created: $swarm_file"
